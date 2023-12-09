2454fb3f931734f0c57dac082dc1750827b19c4b
OraonShivam21 <oraonshivam.0302@gmail.com>
2023-12-09 15:19:19 +0530

    movie search app completed

--- index.js
+++ index.js
@@ -1,26 +1,71 @@
+// main url with apiKey to fetch movie data
 let imdbURL = "https://www.omdbapi.com/";
 let apiKey = "3e9b4576";
 
-window.addEventListener("load", function () {
-  fetchMovieData();
+// search-input and mainSection to display cards
+let movie_search = document.getElementById("movie-search");
+let mainSection = document.getElementById("display-movie-cards");
+
+let searchString = "";
+let timer;
+
+movie_search.addEventListener("input", function () {
+  searchString = movie_search.value;
+  debounceFunction(fetchMovieData, 1000);
 });
 
-async function fetchMovieData(searchString = "a") {
-  let url = `${imdbURL}?apiKey=${apiKey}&s=${searchString}`;
-  try {
-    let res = await fetch(url);
-    let data = await res.json();
-    console.log(data);
-    if(data.Response) {
-        createMovieCard(data.Search);
-    } else {
-        
+function debounceFunction(callback, delay) {
+  if(timer) {
+    clearTimeout(timer);
+  }
+  timer = setTimeout(callback, delay);
+}
+
+async function fetchMovieData() {
+  if(searchString) {
+    let url = `${imdbURL}?apiKey=${apiKey}&s=${searchString}`;
+    try {
+      let res = await fetch(url);
+      let data = await res.json();
+      console.log(data);
+      if(data.Response === "True") {
+        mainSection.classList.remove("display-movie-cards");
+        createMovieCardSection(data.Search);
+      } else if(data.Response === "False") {
+          mainSection.innerHTML = data.Error;
+          mainSection.classList.add("display-movie-cards");
+      }
+    } catch (error) {
+      console.log("Error occurred:", error);
     }
-  } catch (error) {
-    console.log("Error occurred:", error);
+  } else {
+    mainSection.classList.remove("display-movie-cards");
+    mainSection.innerHTML = null;
   }
 }
 
-function createMovieCard(data) {
+function createMovieCardSection(data) {
+  let card_section = `
+    <div class="card-section">
+      ${data.map((item) => {
+        return createMovieCard(item);
+      })
+      .join("")}
+    </div>
+  `;
 
+  mainSection.innerHTML = card_section;
+}
+
+function createMovieCard(data) {
+  let movie_card = `
+    <div class="movie-card">
+      <img class="movie-poster" src=${data.Poster} alt="${data.Title} poster" />
+      <h3 class="movie-title">${data.Title}</h3>
+      <p class="movie-year">Release Year : ${data.Year}</p>
+      <p class="movie-id">IMDB ID : ${data.imdbID}</p>
+    </div>
+  `;
+
+  return movie_card;
 }
